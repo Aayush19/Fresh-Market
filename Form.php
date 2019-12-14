d926a32c41cf7d48b07b96db242415aa739170ff
Aayush19 <aayushkhandelwal4@gmail.com>
2019-12-15 00:56:07 +0530

    Initial

---
+++ Form.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<title>Contact Us</title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="div">
+			<div class="left">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Fruits</a></li>
+					<li><a href="#">Vegetables</a></li>
+				</ul>
+				</div>
+				<div class="right">
+				<ul>
+					<li><a href="login.php">Log In</a></li>
+					<li><a href="login.php">Sign Up</a></li>
+				</ul>
+				</div>
+			</div>
+			<div class="header">
+				<h1>Contact Us</h1>
+				<p>Consumers can now search for product information and place product orders across different regions while online retailers deliver their products directly to the consumers' home.</p>
+			</div>
+			<div class="form">
+				<form>
+					<input type="text" placeholder="Name"/>
+					<input type="text"placeholder="E-Mail"/>
+					<input type="text" id="Message" placeholder="Message"/>
+				</form>
+				<button type="button">Let's Talk</button>
+			</div>
+       </div>
+</body>
+</html>
